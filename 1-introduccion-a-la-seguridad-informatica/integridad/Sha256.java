9dca4d1d696a2d7e895530e3a18063d0aee3647c
Germán Parisi <germannparisi@gmail.com>
2020-10-03 23:43:14 -0300

    Mejoras en el programa sha256

--- 1-introduccion-a-la-seguridad-informatica/integridad/Sha256.java
+++ 1-introduccion-a-la-seguridad-informatica/integridad/Sha256.java
@@ -1,5 +1,6 @@
 import java.security.*;
 import java.nio.charset.*;
+import java.math.*;
 
 public class Sha256 {
    public static void main(String args[]) {
@@ -7,12 +8,22 @@ public class Sha256 {
          MessageDigest generadorHash = MessageDigest.getInstance("SHA-256");
 
          String texto = "CORDOBA CAPITAL";
-         byte[] hash = generadorHash.digest(texto.getBytes(StandardCharsets.UTF_8));
 
-         for (byte i = 0; i < hash.length; i++) {
-            byte h = hash[i];
+         // Se obtiene el hash en bytes.
+         byte[] hashBytes = generadorHash.digest(texto.getBytes(StandardCharsets.UTF_8));
+
+         // Se imprime el resultado en bytes.
+         System.out.println("Resultado en bytes:");
+         for (byte i = 0; i < hashBytes.length; i++) {
+            byte h = hashBytes[i];
             System.out.println(h);
          }
+
+         // Se convierte el hash en formato hexadecimal.
+         System.out.println("Resultado en hexadecimal:");
+         BigInteger hashNumero = new BigInteger(1, hashBytes);
+         String hashString = hashNumero.toString(16);
+         System.out.println(hashString);
       } catch (NoSuchAlgorithmException ex) {
 
       }
